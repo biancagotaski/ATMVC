b801a97d8022cd71635c20d1efe880410724171c
biancagotaski@outlook.com <biancagotaski@outlook.com>
2018-03-27 20:39:31 -0300

    Criando estrutura

---
+++ BLL/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Book
+    {
+        //public Book()
+        //{
+        //    Books 
+        //}
+
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Isbn { get; set; }
+        public int Year { get; set; }
+        public virtual ICollection<Author> Authors { get; set; }
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
---
+++ DAL/Context.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Context : DbContext
+    {
+        public Context()
+            : base("")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+
+            modelBuilder.Entity<Author>()
+                .HasMany(a => a.Books)
+                .WithMany()
+                .Map(x =>
+                {
+                    x.MapLeftKey("AuthorId");
+                    x.MapLeftKey("BookId");
+                    x.ToTable("AuthorBook");
+                });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
